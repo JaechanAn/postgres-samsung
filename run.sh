2b193818eeb3373c22b3b30d19ea7b918953874e
Jaechan An <jaechan5256@gmail.com>
2022-08-09 02:15:59 +0900

    script: sysbench script added

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+if [[ "$1" == "-h" ]]; then
+  echo "Usage: `basename $0` [options]"
+  echo "Options:"
+  echo "  --install           compile and install postgres source"
+  echo "  --initdb            initialize database using initdb command"
+  echo "  --start             start the main server process"
+  echo "  --createdb          create a new user $(whoami), and create a database $(whoami) owned by user $(whoami)"
+  echo "  --stop              terminate the main server process"
+  echo "  --install-sysbench  install sysbench from source"
+  exit 0
+fi
 
 # Directories
 BASE_DIR=$(pwd)
@@ -15,20 +26,24 @@ DATA_DIR="${BASE_DIR}/data"
 
 CONFIG_DIR="${BASE_DIR}/config"
 
+SYSBENCH_DIR="${BASE_DIR}/sysbench"
+
 # Scripts
 INSTALL_SCRIPT="${SCRIPT_DIR}/install.sh"
 INIT_SERVER_SCRIPT="${SCRIPT_DIR}/init_server.sh"
 START_SERVER_SCRIPT="${SCRIPT_DIR}/start_server.sh"
 CREATE_DB_SCRIPT="${SCRIPT_DIR}/create_db.sh"
 STOP_SERVER_SCRIPT="${SCRIPT_DIR}/stop_server.sh"
+INSTALL_SYSBENCH_SCRIPT="${SCRIPT_DIR}/install_sysbench.sh"
+PSQL_SCRIPT="${SCRIPT_DIR}/psql.sh"
 
 LOGFILE="${BASE_DIR}/logfile"
 CONFIGFILE="${CONFIG_DIR}/postgresql.conf"
 CREATE_DB_SQL="${SCRIPT_DIR}/sql/create_db.sql"
 
 # DB Config
-USER="sdb1"
-DATABASE="sdb1"
+USER=$(whoami)
+DATABASE=${USER}
 PORT=5678 # If you change the port, you must change the postgresql.conf file in config directory
 
 # Parse parameters
@@ -60,16 +75,28 @@ do
       shift
       ;;
 
+    --install-sysbench)
+      INSTALL_SYSBENCH=YES
+      shift
+      ;;
+
+    --psql)
+      PSQL=YES
+      shift
+      ;;
+
     *)
       # unknown option
       ;;
   esac
 done
 
-echo "INSTALL  = ${INSTALL}"
-echo "INITDB   = ${INITDB}"
-echo "START    = ${START}"
-echo "CREATEDB = ${CREATEDB}"
+echo "INSTALL           = ${INSTALL}"
+echo "INITDB            = ${INITDB}"
+echo "START             = ${START}"
+echo "CREATEDB          = ${CREATEDB}"
+echo "STOP              = ${STOP}"
+echo "INSTALL_SYSBENCH  = ${INSTALL_SYSBENCH}"
 
 # Install Postgres
 if [[ "${INSTALL}" == "YES" ]]
@@ -118,4 +145,21 @@ then
     ${STOP_SERVER_SCRIPT} \
       --bin-dir=${BIN_DIR} \
       --data-dir=${DATA_DIR}
+fi
+
+# Install Sysbench
+if [[ "${INSTALL_SYSBENCH}" == "YES" ]]
+then
+    ${INSTALL_SYSBENCH_SCRIPT} \
+      --sysbench-base-dir=${SYSBENCH_DIR}
+fi
+
+# Run Client
+if [[ "${PSQL}" == "YES" ]]
+then
+    ${PSQL_SCRIPT} \
+      --bin-dir=${BIN_DIR} \
+      --port=${PORT} \
+      --user=${USER} \
+      --database=${DATABASE}
 fi
